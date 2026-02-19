93595f02e634d168ccae2815130c811c9e8dc7ae
mAzelSk <luisdonado222@gmail.com>
2026-02-19 03:41:14 -0500

    Add comments and documentation to Ejercicio 4.c
    Added comments explaining the purpose of the program and its authors.

--- Ejercicio 4.c
+++ Ejercicio 4.c
@@ -1,3 +1,8 @@
+/* * e puede calcular el cuadrado de un número NUMERO al sumar los primeros NUMERO números impares. Con esta lógica, el cuadrado del número 7 es la sumatoria de los primeros 7 números impares, es decir 1+3+5+7+9+11+13=49. Crear un programa que pida al usuario un número y se almacene en la variable NUMERO y luego imprima la suma de todos los números impares y su resultado.
+ * Integrantes: Angel Higinio, Luis Donado, Yaribeth Rodriguez, Elias Sanchez
+ * Compilador usado: OnlineGDB 
+ */
+
 #include <stdio.h>
 
 int main() {
@@ -24,4 +29,5 @@ int main() {
     printf(" = %d\n", suma);
 
     return 0;
+
 }
