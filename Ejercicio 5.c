3ab9938910666d250a8f40bc17195efcb84c7fe3
mAzelSk <luisdonado222@gmail.com>
2026-02-19 03:40:32 -0500

    Update comments and add new team member

--- Ejercicio 5.c
+++ Ejercicio 5.c
@@ -1,5 +1,5 @@
 /* * Preguntar al usuario un número entero y almacenar en la variable NUMERO. Imprimir el valor de la posición NUMERO de la secuencia de Fibonacci asumiendo que  f_0 y f_1 . Verificar que NUMERO debe ser >= 2, en caso contrario, mostrar un mensaje que NUMERO debe cumplir esa condición.
- * Integrantes: Angel Higinio, Luis Donado, Yaribeth Rodriguez, 
+ * Integrantes: Angel Higinio, Luis Donado, Yaribeth Rodriguez, Elias Sanchez
  * Compilador usado: OnlineGDB 
  */
  
@@ -15,15 +15,12 @@ int main() {
         printf("Error: NUMERO debe ser mayor o igual a 2.\n");
     } else {
         int f0 = 0, f1 = 1, fn;
-
         for (int i = 2; i <= NUMERO; i++) {
             fn = f0 + f1;
             f0 = f1;
             f1 = fn;
         }
-
         printf("El valor en la posicion %d de la secuencia de Fibonacci es: %d\n", NUMERO, f1);
     }
-
     return 0;
 }
